5d3d3cf97cbf2fa25a1872e3d05177aca101e39e
Lucas Chagas de Andrade <49044390+lucaschagas550@users.noreply.github.com>
2022-02-28 23:02:32 -0300

    Feat: Consultas com filtro.

--- src/main/java/br/com/alura/forum/controller/TopicosController.java
+++ src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -1,14 +1,12 @@
 package br.com.alura.forum.controller;
 
 import br.com.alura.forum.controller.dto.TopicoDto;
-import br.com.alura.forum.model.Curso;
 import br.com.alura.forum.model.Topico;
 import br.com.alura.forum.repository.TopicoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Arrays;
 import java.util.List;
 
 @RestController
@@ -18,8 +16,13 @@ public class TopicosController {
     private TopicoRepository topicoRepository;
 
     @RequestMapping("/topicos")
-    public List<TopicoDto> lista(){
-        List<Topico> topicos = topicoRepository.findAll();
+    public List<TopicoDto> lista(String nomeCurso) {
+        if (nomeCurso == null) {
+            List<Topico> topicos = topicoRepository.findAll();
+            return TopicoDto.converter(topicos);
+        }
+
+        List<Topico> topicos = topicoRepository.findByCursoNome(nomeCurso);
         return TopicoDto.converter(topicos);
     }
 }
