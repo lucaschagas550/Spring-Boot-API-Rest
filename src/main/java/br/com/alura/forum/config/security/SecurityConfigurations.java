8a87410429a1f479f7170e2ddb49f30635c9d175
Lucas Chagas de Andrade <49044390+lucaschagas550@users.noreply.github.com>
2022-03-04 19:52:44 -0300

    Feat: Autenticando usuario.

--- src/main/java/br/com/alura/forum/config/security/SecurityConfigurations.java
+++ src/main/java/br/com/alura/forum/config/security/SecurityConfigurations.java
@@ -1,5 +1,7 @@
 package br.com.alura.forum.config.security;
 
+import org.apache.tomcat.util.security.MD5Encoder;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -7,10 +9,23 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.Pbkdf2PasswordEncoder;
+
+import java.sql.SQLOutput;
 
 @EnableWebSecurity
 @Configuration
 public class SecurityConfigurations extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private AutenticacaoService autenticacaoService;
+
+    //Configuracoes de autenticacao
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(autenticacaoService).passwordEncoder(new Pbkdf2PasswordEncoder());
+    }
 
     //Configuração de autorização
     @Override
@@ -23,15 +38,15 @@ public class SecurityConfigurations extends WebSecurityConfigurerAdapter {
 
     }
 
-    //Configuração de autenticação
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-
-    }
-
     //Configuração de recursos estaticos - js, css, imagens, etc..
     @Override
     public void configure(WebSecurity web) throws Exception {
 
     }
+
+  /*  public static void main(String[] args){
+        System.out.println(new Pbkdf2PasswordEncoder().encode("123"));
+    }*/
+
+
 }
