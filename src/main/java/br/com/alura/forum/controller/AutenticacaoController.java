0dcbd3dad82e516e9e6069a122b6854da9b99326
Lucas Chagas de Andrade <49044390+lucaschagas550@users.noreply.github.com>
2022-03-05 11:43:05 -0300

    Feat: Gerando Token com JWT.

--- src/main/java/br/com/alura/forum/controller/AutenticacaoController.java
+++ src/main/java/br/com/alura/forum/controller/AutenticacaoController.java
@@ -1,7 +1,13 @@
 package br.com.alura.forum.controller;
 
+import br.com.alura.forum.config.security.TokenService;
 import br.com.alura.forum.form.LoginForm;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
@@ -10,10 +16,22 @@ import javax.validation.Valid;
 @RequestMapping("/auth")
 public class AutenticacaoController {
 
+    @Autowired
+    private AuthenticationManager authManager; //Precisa ter o metodo authenticationmanager criado na SecurityConfigurations para pode ser injetado aqui
+    @Autowired
+    private TokenService tokenService;
+
     @PostMapping
     public ResponseEntity<?> autenticar(@RequestBody @Valid LoginForm form){
-        System.out.println(form.getEmail());
-        System.out.println(form.getSenha());
-        return ResponseEntity.ok().build();
+        UsernamePasswordAuthenticationToken dadosLogin = form.converter();
+
+        try{
+            Authentication authentication = authManager.authenticate(dadosLogin);
+            String token = tokenService.gerarToken(authentication);
+            System.out.println("Token: " + token);
+            return ResponseEntity.ok().build();
+        }catch (AuthenticationException e){
+            return ResponseEntity.badRequest().build();
+        }
     }
 }
